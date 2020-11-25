85b35a0d932df83f2efad65bbffc6958885f37f7
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-25 16:04:16 -0600

    Created the ProductList module

---
+++ src/components/product/ProductList.js
@@ -0,0 +1,18 @@
+import React, {useContext, useEffect} from "react"
+import { ProductContext } from "./ProductProvider"
+
+export const ProductList = () => {
+    const {products, getProducts} = useContext(ProductContext)
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    return (
+        <div className="products">
+            {
+                products.map(product => <Product key={product.id} product={product} />)
+            }
+        </div>
+    )
+}
